fdd71f1dcec3ab61ed4e5190740a7d27ebf30ded
j1sooo <kjisoo1031@gmail.com>
2025-04-24 15:22:24 +0900

    feat: column nullable 및 builder, update 추가

--- src/main/java/com/est/runtime/post/Post.java
+++ src/main/java/com/est/runtime/post/Post.java
@@ -1,20 +1,35 @@
 package com.est.runtime.post;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor
 @Entity
 public class Post {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String content;
-    private Long likes;
-    private Boolean hidden;
+
+    private Long likes = 0L;
+    private Boolean hidden = true;
+
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
