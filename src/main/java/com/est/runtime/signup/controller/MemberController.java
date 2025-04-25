5ea9cf0d0c776eeb9c763b111930f9d1ec501fe9
BanditKing <plm1414@naver.com>
2025-04-25 14:22:19 +0900

    refactor: 회원가입 API 엔드포인트를 /api/member로 일관성 있게 정비

--- src/main/java/com/est/runtime/signup/controller/MemberController.java
+++ src/main/java/com/est/runtime/signup/controller/MemberController.java
@@ -7,15 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/member/save")
+    @PostMapping("/save")
     public ResponseEntity<String> save(@RequestBody MemberDTO memberDTO) {
-        System.out.println("MemberController save");
-        System.out.println("memberDTO: " + memberDTO);
         memberService.save(memberDTO);
         return ResponseEntity.ok("회원가입 성공");
     }
