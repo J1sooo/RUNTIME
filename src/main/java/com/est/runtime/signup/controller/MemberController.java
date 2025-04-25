d58fade891162d8cc0ccb4631addbac91c2e306a
BanditKing <plm1414@naver.com>
2025-04-25 13:39:54 +0900

    refactor: @Controller → @RestController로 전환 및 ResponseEntity 적용

--- src/main/java/com/est/runtime/signup/controller/MemberController.java
+++ src/main/java/com/est/runtime/signup/controller/MemberController.java
@@ -3,29 +3,20 @@ package com.est.runtime.signup.controller;
 import com.est.runtime.signup.dto.MemberDTO;
 import com.est.runtime.signup.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    // 회원가입 페이지 출력
-    @GetMapping("/member/save")
-    public String saveForm() {
-        return "save";  // save.html
-    }
-
-    // 회원가입 요청 처리
     @PostMapping("/member/save")
-    public String save(@ModelAttribute MemberDTO memberDTO) {
+    public ResponseEntity<String> save(@RequestBody MemberDTO memberDTO) {
         System.out.println("MemberController save");
         System.out.println("memberDTO: " + memberDTO);
         memberService.save(memberDTO);
-        return "index";  // 가입 후 리디렉션할 페이지
+        return ResponseEntity.ok("회원가입 성공");
     }
 }
