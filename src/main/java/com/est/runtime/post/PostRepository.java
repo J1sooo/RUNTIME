b65476e03af91553ea9a04c6f5d2c345d03550ac
j <andcl.island.12@gmail.com>
2025-04-29 11:14:47 +0900

    feat : PostRepository 연관이미지 로딩 추가

--- src/main/java/com/est/runtime/post/PostRepository.java
+++ src/main/java/com/est/runtime/post/PostRepository.java
@@ -1,8 +1,13 @@
 package com.est.runtime.post;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @EntityGraph(attributePaths = "images") // 'images' 연관 엔티티를 즉시 로딩
+    Optional<Post> findById(Long id);
 }
