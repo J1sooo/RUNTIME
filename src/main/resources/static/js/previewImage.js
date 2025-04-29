15a07b8dd16056a4b9acea29e77a1c4fc6de514d
j <andcl.island.12@gmail.com>
2025-04-29 10:22:44 +0900

    feat : articleList, newArticle, postView (HTML), previewimage.js 프론트 푸시 (게시글 조회, 작성)

---
+++ src/main/resources/static/js/previewImage.js
@@ -0,0 +1,27 @@
+document.getElementById("files").addEventListener("change", function(event) {
+    var imagePreviewContainer = document.getElementById("imagePreview");
+    var files = event.target.files;
+
+    // 선택한 이미지들을 미리보기로 추가
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var img = document.createElement("img");
+            img.src = e.target.result;
+            img.classList.add("img-thumbnail");
+            img.style.margin = "5px";
+            img.style.width = "20%";
+            img.style.height = "100%";
+            img.style.objectFit = "contain";
+
+            // 미리보기 컨테이너에 이미지를 추가
+            imagePreviewContainer.appendChild(img);
+        };
+
+        // 이미지를 읽어서 미리보기로 추가
+        reader.readAsDataURL(file);
+    }
+});
+
