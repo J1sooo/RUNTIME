2da2639f723ca898a58062a20826d1e83af51c89
j1sooo <kjisoo1031@gmail.com>
2025-05-09 17:38:49 +0900

    feat: 게시물에서 쪽지 바로가기 기능 구현

--- src/main/java/com/est/runtime/note/NoteViewController.java
+++ src/main/java/com/est/runtime/note/NoteViewController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -31,7 +32,8 @@ public class NoteViewController {
     }
 
     @GetMapping("/note/new")
-    public String getNotes(Model model) {
+    public String postNote(Model model, @RequestParam Long id) {
+        model.addAttribute("id", id);
         model.addAttribute("note", new RequestNote());
         return "newNote";
     }
